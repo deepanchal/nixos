d91bc57359ad8865a44fe3428c5bd4a0993e9c8c
Deep Panchal <deep302001@gmail.com>
2024-03-10 14:38:45 -0500

    fix: remove enableNvidiaPatches option from hyprland config

--- hyprland.nix
+++ hyprland.nix
@@ -4,7 +4,7 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    enableNvidiaPatches = true;
+    # enableNvidiaPatches = true; # No longer needed
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
