d4645c47787dad4bc296e60a0d32894e82e854f5
Deep Panchal <deep302001@gmail.com>
2024-03-16 17:16:16 -0500

    fix(modules): fix nushell config

--- modules/home-manager/shell/starship.nix
+++ modules/home-manager/shell/starship.nix
@@ -34,7 +34,7 @@ in
       enableBashIntegration = config.programs.bash.enable;
       enableZshIntegration = config.programs.zsh.enable;
       enableFishIntegration = config.programs.fish.enable;
-      # enableNushellIntegration = config.programs.nushell.enable;
+      enableNushellIntegration = config.programs.nushell.enable;
 
       enableTransience = config.programs.fish.enable;
 
