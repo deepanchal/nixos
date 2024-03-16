3bf7de1bc484d6672b151f4b15fa3dd8ca599c51
Deep Panchal <deep302001@gmail.com>
2024-03-16 12:58:13 -0500

    feat(hosts): enable asusd & supergfxd services

--- hosts/zephyrion/asus.nix
+++ hosts/zephyrion/asus.nix
@@ -5,8 +5,20 @@
     inputs.nixos-hardware.nixosModules.asus-zephyrus-ga503
   ];
 
+
+  services.asusd = {
+    enable = true;
+    enableUserService = true;
+  };
+
+  services.supergfxd = {
+    enable = true;
+    # settings = {
+    #   
+    # };
+  };
+
   environment.systemPackages = with pkgs; [
     ryzenadj
-    asusctl
   ];
 }
