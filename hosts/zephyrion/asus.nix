892e7640f1d3a569a4cb475fee1d7a0fe0032c28
Deep Panchal <deep302001@gmail.com>
2024-03-15 23:06:04 -0500

    feat(hosts): use nixos-hardware to setup asus config

--- hosts/zephyrion/asus.nix
+++ hosts/zephyrion/asus.nix
@@ -1,39 +1,12 @@
-{ config, pkgs, lib, ... }:
+{ inputs, config, pkgs, lib, ... }:
 
 {
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
-  hardware.opengl.extraPackages = with pkgs; [
-    vaapiVdpau
+  imports = [
+    inputs.nixos-hardware.nixosModules.asus-zephyrus-ga503
   ];
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = lib.mkOverride 990 true;
-      enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true; # Provides `nvidia-offload` command.
-    };
-    # Hardware should specify the bus ID for intel/nvidia devices
-  };
-
-  hardware.nvidia.prime = {
-    amdgpuBusId = "PCI:7:0:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
-
-  # Gnome 40 introduced a new way of managing power, without tlp.
-  # However, these 2 services clash when enabled simultaneously.
-  # https://github.com/NixOS/nixos-hardware/issues/260
-  services.tlp.enable = lib.mkDefault ((lib.versionOlder (lib.versions.majorMinor lib.version) "21.05")
-                                       || !config.services.power-profiles-daemon.enable);
-
-  # For ssd
-  services.fstrim.enable = lib.mkDefault true;
-
-  # fixes mic mute button
-  services.udev.extraHwdb = ''
-    evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
-     KEYBOARD_KEY_ff31007c=f20
-  '';
+  environment.systemPackages = with pkgs; [
+    ryzenadj
+    asusctl
+  ];
 }
