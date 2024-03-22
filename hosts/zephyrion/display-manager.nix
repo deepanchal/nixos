1b063b6668fcf572b6aa67fcdf9a774da4259234
Deep Panchal <deep302001@gmail.com>
2024-03-22 17:39:32 -0500

    fix(hosts): change display manager settings

--- hosts/zephyrion/display-manager.nix
+++ hosts/zephyrion/display-manager.nix
@@ -1,18 +1,44 @@
 { pkgs, ... }:
 
 {
+  services.xserver.displayManager.gdm.enable = true;
+
   # Enable Display Manager
   # services.greetd = {
   #   enable = true;
   #   settings = {
+  #     # default_session = {
+  #     #   command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+  #     #   user = "greeter";
+  #     # };
   #     default_session = {
-  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+  #       command = "${pkgs.cage}/bin/cage -s ${pkgs.greetd.gtkgreet}/bin/gtkgreet";
   #       user = "greeter";
   #     };
   #   };
   # };
-  #
-  # environment.systemPackages = with pkgs; [
-  #   greetd.tuigreet
-  # ];
+
+  # services.greetd.enable = true;
+  # programs.regreet = {
+  #   enable = true;
+  #   package = pkgs.greetd.regreet;
+  #   settings = {
+  #     cageArgs = [
+  #       "-s"
+  #       "-m"
+  #       "last"
+  #     ];
+  #     commands = {
+  #       reboot = [ "systemctl" "reboot" ];
+  #       poweroff = [ "systemctl" "poweroff" ];
+  #     };
+  #   };
+  # };
+
+  environment.systemPackages = with pkgs; [
+    # cage
+    # greetd.tuigreet
+    # greetd.gtkgreet
+    # greetd.regreet
+  ];
 }
--- hosts/zephyrion/gnome.nix
+++ hosts/zephyrion/gnome.nix
@@ -3,7 +3,6 @@
 {
   # Enable Gnome
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Gnome Exclude Packages
