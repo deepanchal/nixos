15361638c5472e9d24a3a1f265e506662f555ffd
Deep Panchal <deep302001@gmail.com>
2024-03-21 20:29:46 -0500

    feat(hosts): enable nm-applet in networking

--- hosts/zephyrion/networking.nix
+++ hosts/zephyrion/networking.nix
@@ -6,6 +6,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  programs.nm-applet.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
