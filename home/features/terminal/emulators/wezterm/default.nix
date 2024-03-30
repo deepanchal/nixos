066a49a75e8ed7a1a964c0929711d33d1506856f
Deep Panchal <deep302001@gmail.com>
2024-03-30 17:40:38 -0500

    feat(home): add scrollback editor & other keybinds in wezterm config

--- home/features/terminal/emulators/wezterm/default.nix
+++ home/features/terminal/emulators/wezterm/default.nix
@@ -39,6 +39,39 @@ in
     #   };
     # };
     extraConfig = /* lua */ ''
+      local io = require 'io'
+      local os = require 'os'
+      local act = wezterm.action
+
+      wezterm.on('trigger-vim-with-scrollback', function(window, pane)
+        -- Retrieve the text from the pane
+        local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+        -- Create a temporary file to pass to vim
+        local name = os.tmpname()
+        local f = io.open(name, 'w+')
+        f:write(text)
+        f:flush()
+        f:close()
+
+        -- Open a new window running vim and tell it to open the file
+        window:perform_action(
+          act.SpawnCommandInNewWindow {
+            args = { 'vim', name },
+          },
+          pane
+        )
+
+        -- Wait "enough" time for vim to read the file before we remove it.
+        -- The window creation and process spawn are asynchronous wrt. running
+        -- this script and are not awaitable, so we just pick a number.
+        --
+        -- Note: We don't strictly need to remove this file, but it is nice
+        -- to avoid cluttering up the temporary directory.
+        wezterm.sleep_ms(1000)
+        os.remove(name)
+      end)
+      
       return {
         color_scheme = "${colorscheme.slug}",
 
@@ -70,6 +103,21 @@ in
         --   'FiraCode Nerd Font',
         --   'Noto Color Emoji',
         -- },
+
+        keys = {
+          {
+            key = 'E',
+            mods = 'CTRL|SHIFT',
+            action = act.EmitEvent 'trigger-vim-with-scrollback',
+          },
+          {
+            key = 'Y',
+            mods = 'CTRL|SHIFT',
+            action = act.SpawnCommandInNewWindow {
+              args = { 'btop' },
+            },
+          },
+        },
       };
     '';
   };
