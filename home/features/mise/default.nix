ee0d6d9931fd4432e6a8373cf5e54e6afb8ad9cb
Deep Panchal <deep302001@gmail.com>
2024-04-07 10:42:37 -0500

    feat(home): add mise config
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

---
+++ home/features/mise/default.nix
@@ -0,0 +1,31 @@
+{config, ...}: {
+  programs.mise = {
+    enable = true;
+    enableBashIntegration = config.programs.bash.enable;
+    enableZshIntegration = config.programs.zsh.enable;
+    enableFishIntegration = config.programs.fish.enable;
+    globalConfig = {
+      tools = {
+        # node = "lts";
+        # rust = "stable";
+        pnpm = "latest";
+        # python = "latest";
+        # java = [
+        #   "adoptopenjdk-17.0.8+7"
+        # ];
+        # python = [
+        #   "3.11"
+        # ];
+      };
+      alias = {};
+    };
+    settings = {
+      verbose = false;
+      experimental = true;
+      all_compile = false;
+      python_compile = false;
+      node_compile = false;
+      disable_tools = [];
+    };
+  };
+}
