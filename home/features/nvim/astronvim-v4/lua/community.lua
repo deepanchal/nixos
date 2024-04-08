b42e873af828e5fced240315f4fc0fde391ecbc5
Deep Panchal <deep302001@gmail.com>
2024-04-08 12:12:54 -0500

    feat(home.nvim): add useful astrocommunity plugin packs
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/features/nvim/astronvim-v4/lua/community.lua
+++ home/features/nvim/astronvim-v4/lua/community.lua
@@ -7,6 +7,42 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- Themes
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  -- { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+
+  -- Color
+  { import = "astrocommunity.color.transparent-nvim" },
+
+  -- Motion
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.harpoon" },
+
+  -- Git
+  { import = "astrocommunity.git.git-blame-nvim" },
+
+  -- Diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  -- Plugin Packs
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
+
+  -- Lsp
+  { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
 }
