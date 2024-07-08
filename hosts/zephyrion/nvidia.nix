e0af7bacf5e603f7dc24297c4e98e6c2a86f1b4c
Deep Panchal <deep302001@gmail.com>
2024-07-07 22:31:07 -0500

    fix(hosts): update nvidia.nix for zephyrion
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- hosts/zephyrion/nvidia.nix
+++ hosts/zephyrion/nvidia.nix
@@ -14,7 +14,7 @@
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    driSupport = true;
+    #driSupport = true;
     driSupport32Bit = true;
     extraPackages = with pkgs; [
       vaapiVdpau
