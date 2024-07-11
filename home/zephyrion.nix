2d58b40fb2477b9bd5c7a0d391d40def686b00df
Deep Panchal <deep302001@gmail.com>
2024-07-10 20:47:44 -0500

    feat(home): install default nerdfonts for user
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/zephyrion.nix
+++ home/zephyrion.nix
@@ -66,7 +66,7 @@ in {
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
