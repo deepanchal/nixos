3ee42c7c6f23a4be1ae85955260f0b56cde13c41
Deep Panchal <deep302001@gmail.com>
2024-07-12 18:57:53 -0500

    fix(yazi): fix yazi shell wrapper not working correctly
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/features/terminal/programs/yazi.nix
+++ home/features/terminal/programs/yazi.nix
@@ -11,27 +11,20 @@
 in {
   home.packages = [
     pkgs.exiftool
-    # https://yazi-rs.github.io/docs/quick-start#shell-wrapper
-    (pkgs.writeShellScriptBin "f" ''
-      set -ue -o pipefail
-
-      function f() {
-	      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	      yazi "$@" --cwd-file="$tmp"
-	      if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		      cd -- "$cwd"
-	      fi
-	      rm -f -- "$tmp"
-      }
-
-      f "$@"
-    '')
   ];
   xdg.configFile."yazi/Catppuccin-${lib.toLower flavor}.tmTheme".source = "${catppuccin-bat}/themes/Catppuccin ${flavor}.tmTheme";
 
   programs.yazi = {
     enable = true;
+    enableBashIntegration = config.programs.bash.enable;
+    enableZshIntegration = config.programs.zsh.enable;
+    enableFishIntegration = config.programs.fish.enable;
+    enableNushellIntegration = config.programs.nushell.enable;
+
     theme = lib.importTOML "${catppuccin-yazi}/themes/${lib.toLower flavor}.toml";
+    # Launches yazi and drops in current dir on exit
+    # See: https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
+    shellWrapperName = "f";
     settings = {
       manager = {
         layout = [1 4 3];
@@ -51,13 +44,25 @@ in {
       };
     };
     keymap = {
-      manager.append_keymap = [
+      manager.prepend_keymap = [
         {
           on = ["d"];
           run = [
             "escape --visual"
             ''shell --confirm 'trashy put "$@"' ''
           ];
+        }
+        {
+          on = ["<C-s>"];
+          run = "shell \"$SHELL\" --block --confirm";
+          desc = "Open shell here";
+        }
+      ];
+      input.prepend_keymap = [
+        {
+          on = ["<Esc>"];
+          run = "close";
+          desc = "Cancel input";
         }
       ];
     };
