5d12139f8c40c317e00c6d936d38d7695f024b8c
Deep Panchal <deep302001@gmail.com>
2024-08-04 09:49:56 -0500

    feat(hosts): add postCreateHook to take blank root snapshot for main btrfs root subvol
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- hosts/zephyrion/disk-config.nix
+++ hosts/zephyrion/disk-config.nix
@@ -44,6 +44,13 @@
               content = {
                 type = "btrfs";
                 extraArgs = ["-f" "-L NIXOS"];
+                postCreateHook = ''
+                  # Take blank root snapshot for impermanence / fs-diff
+                  MNTPOINT=$(mktemp -d)
+                  mount "/dev/disk/by-label/NIXOS" "$MNTPOINT" -o subvol=/
+                  trap 'umount $MNTPOINT; rm -rf $MNTPOINT' EXIT
+                  btrfs subvolume snapshot -r $MNTPOINT/@ $MNTPOINT/root-blank
+                '';
                 subvolumes = {
                   # mount the top-level subvolume at /btr_pool
                   # it will be used by btrbk to create snapshots
