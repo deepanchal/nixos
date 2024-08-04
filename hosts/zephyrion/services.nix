8ed9aae95fcdee491abbe30593914aaf78d4c15b
Deep Panchal <deep302001@gmail.com>
2024-08-04 12:43:58 -0500

    fix(hosts): change nh helper flake path
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- hosts/zephyrion/services.nix
+++ hosts/zephyrion/services.nix
@@ -84,7 +84,7 @@ in {
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/deep/nixos";
+    flake = "/home/deep/projects/nixos";
   };
 
   # Enable CUPS to print documents.
