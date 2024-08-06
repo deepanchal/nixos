a51ed26afefcc502510ebd4428034f2aedd7805c
Deep Panchal <deep302001@gmail.com>
2024-08-05 20:45:25 -0500

    feat(home): enable command-not-found & awscli in misc.nix
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/features/terminal/programs/misc.nix
+++ home/features/terminal/programs/misc.nix
@@ -7,4 +7,19 @@
 }: {
   programs.jq.enable = true;
   programs.taskwarrior.enable = true;
+  programs.command-not-found.enable = true; # https://discourse.nixos.org/t/which-package-is-mkfs-in/24882/3
+  programs.awscli = {
+    enable = true;
+    credentials = {
+      # "default" = {
+      #   "credential_process" = "${pkgs.pass}/bin/pass show aws";
+      # };
+    };
+    settings = {
+      "default" = {
+        # region = "eu-west-3";
+        output = "json";
+      };
+    };
+  };
 }
