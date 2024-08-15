1d47fcbf8a5588c173b4c9d4d54c74acd2ceafb1
Deep Panchal <deep302001@gmail.com>
2024-08-14 20:26:14 -0500

    feat(home): update waybar theme & config
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/features/theme/default.nix
+++ home/features/theme/default.nix
@@ -1,24 +1,39 @@
 {
-  # convert rrggbb hex to #rrggbb
-  x = c: "#${c}";
+  wallpaper = ./wallpapers/Rainnight.jpg;
 
-  # scheme: "Catppuccin Macchiato"
-  # base00: "#24273a" # base
-  # base01: "#1e2030" # mantle
-  # base02: "#363a4f" # surface0
-  # base03: "#494d64" # surface1
-  # base04: "#5b6078" # surface2
-  # base05: "#cad3f5" # text
-  # base06: "#f4dbd6" # rosewater
-  # base07: "#b7bdf8" # lavender
-  # base08: "#ed8796" # red
-  # base09: "#f5a97f" # peach
-  # base0A: "#eed49f" # yellow
-  # base0B: "#a6da95" # green
-  # base0C: "#8bd5ca" # teal
-  # base0D: "#8aadf4" # blue
-  # base0E: "#c6a0f6" # mauve
-  # base0F: "#f0c6c6" # flamingo
+  # ***********************************
+  # Catppuccin Mocha Theme
+  # ***********************************
+  # --------------------------------------------------------------------------
+  # | Labels    | base16 | Hex     | RGB                | HSL                |
+  # |-----------|--------|---------|--------------------|--------------------|
+  # | Rosewater | base06 | #f5e0dc | rgb(245, 224, 220) | hsl(10, 56%, 91%)  |
+  # | Flamingo  | base0F | #f2cdcd | rgb(242, 205, 205) | hsl(0, 59%, 88%)   |
+  # | Pink      |        | #f5c2e7 | rgb(245, 194, 231) | hsl(316, 72%, 86%) |
+  # | Mauve     | base0E | #cba6f7 | rgb(203, 166, 247) | hsl(267, 84%, 81%) |
+  # | Red       | base08 | #f38ba8 | rgb(243, 139, 168) | hsl(343, 81%, 75%) |
+  # | Maroon    |        | #eba0ac | rgb(235, 160, 172) | hsl(350, 65%, 77%) |
+  # | Peach     | base09 | #fab387 | rgb(250, 179, 135) | hsl(23, 92%, 75%)  |
+  # | Yellow    | base0A | #f9e2af | rgb(249, 226, 175) | hsl(41, 86%, 83%)  |
+  # | Green     | base0B | #a6e3a1 | rgb(166, 227, 161) | hsl(115, 54%, 76%) |
+  # | Teal      | base0C | #94e2d5 | rgb(148, 226, 213) | hsl(170, 57%, 73%) |
+  # | Sky       |        | #89dceb | rgb(137, 220, 235) | hsl(189, 71%, 73%) |
+  # | Sapphire  |        | #74c7ec | rgb(116, 199, 236) | hsl(199, 76%, 69%) |
+  # | Blue      | base0D | #89b4fa | rgb(137, 180, 250) | hsl(217, 92%, 76%) |
+  # | Lavender  | base07 | #b4befe | rgb(180, 190, 254) | hsl(232, 97%, 85%) |
+  # | Text      | base05 | #cdd6f4 | rgb(205, 214, 244) | hsl(226, 64%, 88%) |
+  # | Subtext1  |        | #bac2de | rgb(186, 194, 222) | hsl(227, 35%, 80%) |
+  # | Subtext0  |        | #a6adc8 | rgb(166, 173, 200) | hsl(228, 24%, 72%) |
+  # | Overlay2  |        | #9399b2 | rgb(147, 153, 178) | hsl(228, 17%, 64%) |
+  # | Overlay1  |        | #7f849c | rgb(127, 132, 156) | hsl(230, 13%, 55%) |
+  # | Overlay0  |        | #6c7086 | rgb(108, 112, 134) | hsl(231, 11%, 47%) |
+  # | Surface2  | base04 | #585b70 | rgb(88, 91, 112)   | hsl(233, 12%, 39%) |
+  # | Surface1  | base03 | #45475a | rgb(69, 71, 90)    | hsl(234, 13%, 31%) |
+  # | Surface0  | base02 | #313244 | rgb(49, 50, 68)    | hsl(237, 16%, 23%) |
+  # | Base      | base00 | #1e1e2e | rgb(30, 30, 46)    | hsl(240, 21%, 15%) |
+  # | Mantle    | base01 | #181825 | rgb(24, 24, 37)    | hsl(240, 21%, 12%) |
+  # | Crust     |        | #11111b | rgb(17, 17, 27)    | hsl(240, 23%, 9%)  |
+  # --------------------------------------------------------------------------
 
   # scheme: "Catppuccin Mocha"
   # base00: "#1e1e2e" # base
@@ -37,37 +52,4 @@
   # base0D: "#89b4fa" # blue
   # base0E: "#cba6f7" # mauve
   # base0F: "#f2cdcd" # flamingo
-
-  colors = rec {
-    rosewater = "f2d5cf";
-    flamingo = "eebebe";
-    pink = "f4b8e4";
-    mauve = "ca9ee6";
-    red = "e78284";
-    maroon = "ea999c";
-    peach = "ef9f76";
-    yellow = "e5c890";
-    green = "a6d189";
-    teal = "81c8be";
-    sky = "99d1db";
-    sapphire = "85c1dc";
-    blue = "8caaee";
-    lavender = "babbf1";
-    text = "c6d0f5";
-    subtext1 = "b5bfe2";
-    subtext0 = "a5adce";
-    overlay2 = "949cbb";
-    overlay1 = "838ba7";
-    overlay0 = "737994";
-    surface2 = "626880";
-    surface1 = "51576d";
-    surface0 = "414559";
-    base = "303446";
-    mantle = "292c3c";
-    crust = "232634";
-
-    accent = teal;
-  };
-
-  wallpaper = ./wallpapers/Rainnight.jpg;
 }
