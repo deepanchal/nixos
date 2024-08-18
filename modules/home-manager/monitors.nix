13ed86c6b4153f6e12e474550ba74fba8ca13aca
Deep Panchal <deep302001@gmail.com>
2024-08-18 14:50:12 -0500

    feat(modules): add monitors hm module
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

---
+++ modules/home-manager/monitors.nix
@@ -0,0 +1,64 @@
+# From: https://github.com/Misterio77/nix-config/blob/99e055eb0a753231d2ae1d64011a3420467778c5/modules/home-manager/monitors.nix
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkOption types;
+in {
+  options.monitors = mkOption {
+    type = types.listOf (
+      types.submodule {
+        options = {
+          name = mkOption {
+            type = types.str;
+            example = "DP-1";
+          };
+          enabled = mkOption {
+            type = types.bool;
+            default = true;
+          };
+          primary = mkOption {
+            type = types.bool;
+            default = false;
+          };
+          width = mkOption {
+            type = types.int;
+            example = 1920;
+          };
+          height = mkOption {
+            type = types.int;
+            example = 1080;
+          };
+          x = mkOption {
+            type = types.int;
+            default = 0;
+          };
+          y = mkOption {
+            type = types.int;
+            default = 0;
+          };
+          refreshRate = mkOption {
+            type = types.float;
+            default = 60;
+          };
+          scaleFactor = mkOption {
+            type = types.float;
+            default = 1;
+          };
+        };
+      }
+    );
+    default = [];
+  };
+  config = {
+    assertions = [
+      {
+        assertion =
+          ((lib.length config.monitors) != 0)
+          -> ((lib.length (lib.filter (m: m.primary) config.monitors)) == 1);
+        message = "Exactly one monitor must be set to primary.";
+      }
+    ];
+  };
+}
