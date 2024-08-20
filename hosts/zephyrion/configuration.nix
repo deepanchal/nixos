8da5057db6bd3eb8516bcadc48b25373aa34a32b
Deep Panchal <deep302001@gmail.com>
2024-08-19 19:42:38 -0500

    fix(hosts): remove nix gc & auto update as it's handled by nh
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- hosts/zephyrion/configuration.nix
+++ hosts/zephyrion/configuration.nix
@@ -151,40 +151,6 @@
   };
 
   ##################################################
-  # NIX GARBAGE COLLECTION
-  ##################################################
-  # Garbage collection
-  # Optimize storage and automatic scheduled GC running
-  # If you want to run GC manually, use commands:
-  # `nix-store --optimize` for finding and eliminating redundant copies of identical store paths
-  # `nix-store --gc` for optimizing the nix store and removing unreferenced and obsolete store paths
-  # `nix-collect-garbage -d` for deleting old generations of user profiles
-  ##################################################
-  nix.settings.auto-optimise-store = true;
-  nix.optimise.automatic = true;
-  nix.gc = {
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-
-  ##################################################
-  # NIX AUTO UPGRADE
-  ##################################################
-  # Scheduled auto upgrade system (this is only for system upgrades,
-  # if you want to upgrade cargo\npm\pip global packages, docker containers or different part of the system
-  # or get really full system upgrade, use `topgrade` CLI utility manually instead.
-  # I recommend running `topgrade` once a week or at least once a month)
-  ##################################################
-  system.autoUpgrade = {
-    enable = false;
-    operation = "switch"; # If you don't want to apply updates immediately, only after rebooting, use `boot` option in this case
-    flake = "/etc/nixos";
-    flags = ["--update-input" "nixpkgs" "--update-input" "rust-overlay" "--commit-lock-file"];
-    dates = "weekly";
-    # channel = "https://nixos.org/channels/nixos-unstable";
-  };
-
-  ##################################################
   # NETWORKING
   ##################################################
   networking = {
