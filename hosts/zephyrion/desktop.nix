2dd261d2633c91b0a410cf47ad5daf8df606ae37
Deep Panchal <deep302001@gmail.com>
2024-09-01 20:01:06 -0500

    fix(hosts): fix duplicate session entries in tuigreet
    - Closes #27
    
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- hosts/zephyrion/desktop.nix
+++ hosts/zephyrion/desktop.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   inputs,
@@ -67,9 +68,13 @@
     settings = {
       default_session = let
         tuigreet = "${lib.getExe pkgs.greetd.tuigreet}";
+        baseSessionsDir = "${config.services.xserver.displayManager.sessionData.desktops}";
+        xSessions = "${baseSessionsDir}/share/xsessions";
+        waylandSessions = "${baseSessionsDir}/share/wayland-sessions";
         tuigreetOptions = [
           "--remember"
           "--remember-session"
+          "--sessions ${waylandSessions}:${xSessions}"
           "--time"
           # Make sure theme is wrapped in single quotes. See https://github.com/apognu/tuigreet/issues/147
           "--theme 'border=blue;text=cyan;prompt=green;time=red;action=blue;button=white;container=black;input=red'"
