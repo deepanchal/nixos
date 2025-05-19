fbd3f105e16a00870537d1716163a39a24dc57c2
Deep Panchal <deep302001@gmail.com>
2025-05-19 16:57:03 -0500

    fix(system): upgrade system flake & pkgs
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=220c3093097aa7a53525edd46e4bac3672458210";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=d025eece9b65b33ba7b983e0df1fe529045a3f6a";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur = {
       url = "github:nix-community/NUR";
