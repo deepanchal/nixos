069af05a41fe12c2ae666e9767e13ff89ef88826
Deep Panchal <deep302001@gmail.com>
2025-08-09 16:11:07 -0500

    fix(home): fix gtk theme, use magnetic-catppuccin-gtk instead since official theme is deprecated
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/features/desktop/gtk.nix
+++ home/features/desktop/gtk.nix
@@ -3,7 +3,8 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   dconf = {
     enable = true;
     settings = {
@@ -15,6 +16,15 @@
 
   gtk = {
     enable = true;
+    theme = {
+      name = "Catppuccin-GTK-Purple-Dark-Compact";
+      package = pkgs.magnetic-catppuccin-gtk.override {
+        accent = [ "purple" ]; # "default" | "purple" | "pink" | "red" | "orange" | "yellow" | "green" | "teal" | "grey" | "all"
+        shade = "dark"; # "light" | "dark"
+        size = "compact"; # "standard" | "compact"
+        # tweaks = [ "macchiato" ]; # "frappe" | "macchiato" | "black" | "float" | "outline" | "macos"
+      };
+    };
     cursorTheme = {
       name = config.home.pointerCursor.name;
       size = config.home.pointerCursor.size;
@@ -25,17 +35,19 @@
     };
     gtk3 = {
       # Bookmarks in the sidebar of GTK file browser
-      bookmarks = let
-        userHome = config.home.homeDirectory;
-      in [
-        "file:///tmp"
-        "file://${userHome}/projects"
-        "file://${userHome}/Pictures"
-        "file://${userHome}/Downloads"
-        "file://${userHome}/Documents"
-        "file:///btr_pool"
-        "file:///btr_pool/@dumps"
-      ];
+      bookmarks =
+        let
+          userHome = config.home.homeDirectory;
+        in
+        [
+          "file:///tmp"
+          "file://${userHome}/projects"
+          "file://${userHome}/Pictures"
+          "file://${userHome}/Downloads"
+          "file://${userHome}/Documents"
+          "file:///btr_pool"
+          "file:///btr_pool/@dumps"
+        ];
     };
   };
 
