9d679689a47b34d02d33767bdcd7ed87878eb61d
Deep Panchal <deep302001@gmail.com>
2025-12-07 10:45:44 -0600

    feat(nvim): add claude-code nvim plugin to astronvim
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- home/features/editors/nvim/astronvim/lua/plugins/user.lua
+++ home/features/editors/nvim/astronvim/lua/plugins/user.lua
@@ -141,6 +141,14 @@ return {
     "delphinus/vim-firestore",
   },
 
+  {
+    "greggh/claude-code.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function() require("claude-code").setup() end,
+  },
+
   ---------------------------------------
   -- Astrocommunity overrides
   ---------------------------------------
